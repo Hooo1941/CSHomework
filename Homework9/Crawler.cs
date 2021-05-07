6473dafcd28f7166265a467e857a007f291d8348
Hooo1941 <jiahao1941@outlook.com>
2021-05-07 17:20:29 +0800

    Submit homework9

---
+++ Homework9/Crawler.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace crawler
+{
+    class Info
+    {
+        public string Url { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; }
+        public string FileName { get; set; }
+        public Info(string url)
+        {
+            Url = url;
+        }
+    }
+
+    class SimpleCrawler
+    {
+        public int MaxCount = 10;
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        public List<Info> InfoList;
+        public bool IsCrawling = false;
+        public string BaseUrl;
+        public void Crawl()
+        {
+            count = 0;
+            InfoList = new List<Info>();
+            IsCrawling = true;
+            urls = new Hashtable {{BaseUrl, false}};
+            var uri = new Uri(BaseUrl);
+            BaseUrl = uri.Host;
+            while (true)
+            {
+                string current = null;
+                foreach (string url in urls.Keys)
+                {
+                    if ((bool)urls[url]) continue;
+                    current = url;
+                }
+
+                if (current == null || count > MaxCount) break;
+                InfoList.Add(new Info(current));
+                string html;
+                try
+                {
+                    html = DownLoad(current); // 下载
+                }
+                catch (Exception e)
+                {
+                    InfoList[InfoList.Count - 1].Success = false;
+                    InfoList[InfoList.Count - 1].Error = e.ToString();
+                    urls[current] = true;
+                    continue;
+                }
+
+                InfoList[InfoList.Count - 1].Success = true;
+                InfoList[InfoList.Count - 1].FileName = count.ToString() + ".html";
+                urls[current] = true;
+                count++;
+                Parse(current, html);//解析,并加入新的链接
+                //Console.WriteLine("爬行结束");
+            }
+
+            IsCrawling = false;
+            urls = new Hashtable();
+        }
+
+        public string DownLoad(string url)
+            {
+            var webClient = new WebClient {Encoding = Encoding.UTF8};
+            var html = webClient.DownloadString(url);
+            var fileName = count.ToString() + ".html";
+            File.WriteAllText(fileName, html, Encoding.UTF8); 
+            return html;
+        }
+
+        private void Parse(string current, string html)
+        {
+            var strRef = @"(href|HREF)[]*=[]*[""'][^""'#>]+[""']";
+            var matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1)
+                    .Trim('"', '\"', '#', '>');
+                if (strRef.Length == 0) continue;
+                if (!strRef.Contains("htm") && !strRef.Contains("aspx") && !strRef.Contains("jsp")) continue;
+                if (strRef[0] == '/') strRef = Path.Combine(BaseUrl, strRef); // 相对路径，/开始，拼接域名。
+                else if (strRef.Contains("http") == false) strRef = Path.Combine(current, strRef); // 相对路径，没有/开始，拼接正在打开的网页。
+                if (strRef.Contains(BaseUrl) == false) continue; // 不是指定的网站
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+    }
+}
---
+++ Homework9/CrawlerView.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace crawler
+{
+    partial class CrawlerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbState = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.urlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.successDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.errorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fileNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bdsInfo = new System.Windows.Forms.BindingSource(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudMaxCount = new System.Windows.Forms.NumericUpDown();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsInfo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.nudMaxCount);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.lbState);
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 42);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(628, 9);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(97, 24);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "开始爬取";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbState
+            // 
+            this.lbState.AutoSize = true;
+            this.lbState.Location = new System.Drawing.Point(731, 14);
+            this.lbState.Name = "lbState";
+            this.lbState.Size = new System.Drawing.Size(41, 12);
+            this.lbState.TabIndex = 2;
+            this.lbState.Text = "未开始";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(89, 10);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(316, 21);
+            this.txtURL.TabIndex = 1;
+            this.txtURL.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "初始URL：";
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.AllowUserToAddRows = false;
+            this.dgvInfo.AllowUserToDeleteRows = false;
+            this.dgvInfo.AutoGenerateColumns = false;
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.urlDataGridViewTextBoxColumn,
+            this.successDataGridViewCheckBoxColumn,
+            this.errorDataGridViewTextBoxColumn,
+            this.fileNameDataGridViewTextBoxColumn});
+            this.dgvInfo.DataSource = this.bdsInfo;
+            this.dgvInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvInfo.Location = new System.Drawing.Point(0, 42);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.ReadOnly = true;
+            this.dgvInfo.RowTemplate.Height = 23;
+            this.dgvInfo.Size = new System.Drawing.Size(800, 408);
+            this.dgvInfo.TabIndex = 1;
+            this.dgvInfo.DataError += new System.Windows.Forms.DataGridViewDataErrorEventHandler(this.dgvInfo_DataError);
+            // 
+            // urlDataGridViewTextBoxColumn
+            // 
+            this.urlDataGridViewTextBoxColumn.DataPropertyName = "Url";
+            this.urlDataGridViewTextBoxColumn.HeaderText = "Url";
+            this.urlDataGridViewTextBoxColumn.Name = "urlDataGridViewTextBoxColumn";
+            this.urlDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // successDataGridViewCheckBoxColumn
+            // 
+            this.successDataGridViewCheckBoxColumn.DataPropertyName = "Success";
+            this.successDataGridViewCheckBoxColumn.HeaderText = "Success";
+            this.successDataGridViewCheckBoxColumn.Name = "successDataGridViewCheckBoxColumn";
+            this.successDataGridViewCheckBoxColumn.ReadOnly = true;
+            // 
+            // errorDataGridViewTextBoxColumn
+            // 
+            this.errorDataGridViewTextBoxColumn.DataPropertyName = "Error";
+            this.errorDataGridViewTextBoxColumn.HeaderText = "Error";
+            this.errorDataGridViewTextBoxColumn.Name = "errorDataGridViewTextBoxColumn";
+            this.errorDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fileNameDataGridViewTextBoxColumn
+            // 
+            this.fileNameDataGridViewTextBoxColumn.DataPropertyName = "FileName";
+            this.fileNameDataGridViewTextBoxColumn.HeaderText = "FileName";
+            this.fileNameDataGridViewTextBoxColumn.Name = "fileNameDataGridViewTextBoxColumn";
+            this.fileNameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // bdsInfo
+            // 
+            this.bdsInfo.DataSource = typeof(crawler.Info);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(420, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "最多爬取个数：";
+            // 
+            // nudMaxCount
+            // 
+            this.nudMaxCount.Location = new System.Drawing.Point(515, 11);
+            this.nudMaxCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMaxCount.Name = "nudMaxCount";
+            this.nudMaxCount.Size = new System.Drawing.Size(88, 21);
+            this.nudMaxCount.TabIndex = 5;
+            this.nudMaxCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // CrawlerView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.panel1);
+            this.Name = "CrawlerView";
+            this.Text = "CrawlerView";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bdsInfo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMaxCount)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lbState;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.BindingSource bdsInfo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn successDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn errorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fileNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown nudMaxCount;
+        private System.Windows.Forms.Label label2;
+    }
+}
